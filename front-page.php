2e04e1ed2aad021d6e21525bb0806d99dd4f3381
Rob van Groenewoud <git@robvangroenewoud.com>
2013-02-11 22:56:17 +0100

    Update royal slider id

--- front-page.php
+++ front-page.php
@@ -3,7 +3,11 @@
 <?php the_post(); ?>
 
 <div class="swiper-area">
-	<?php echo get_new_royalslider(2); ?>
+
+	<?php 
+	// Determine royal slider id properly
+	echo get_new_royalslider(1); 
+	?>
 </div>
 
 <?php the_content(); ?>
